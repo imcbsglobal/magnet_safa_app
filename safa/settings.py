9bd2b727be21a0c371ccff371d920a0330d65070
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-06-18 17:33:28 +0530

    settings conflict static file DIR removed:

--- safa/settings.py
+++ safa/settings.py
@@ -133,9 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'school', 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'school', 'static'),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
 
 # Default primary key field type
